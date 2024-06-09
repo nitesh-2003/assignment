eda748da6f47edd8efe6f10bf5e4310b32b655bf
nitesh-2003 <niteeshkumar613@gmail.com>
2024-06-09 14:57:22 +0530

    new changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask , render_template, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def index():    
     return "hello world"
 
 @app.route("/login")
@@ -13,7 +13,13 @@ def login():
 def register():
     return render_template("signup.html")
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
-
+@app.route("/writer")
+def become_writer():
+    return render_template("becomewriter.html")
+        
 if __name__ == '__main__':
     app.run(debug=True)
